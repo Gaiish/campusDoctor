1f6dbb2888b07816bbd368b807d167d0b3966931
Gaiish <gdishim@gmail.com>
2019-05-12 09:19:19 +0300

    create function for the periods of dates to be marked on the calendar

--- screens/period/PeriodScreen.js
+++ screens/period/PeriodScreen.js
@@ -8,11 +8,24 @@ var moment = require('moment')
 
 
 export default class PeriodScreen extends Component {
-  state = {
-    numofperiod: 0,
-    lengthofperiod: '',
-    perioddate: '',
+  constructor(){
+    super()
+    this.state = {
+      numofperiod: 0,
+      lengthofperiod: '',
+      perioddate: '',
+    }
+
+
+    //here we have to find a way to pass all dates we want to mark at once
+    //since markedDates prop accepts an immutable object, which means you can't
+    //change it over time or pass to it something from the state
+    this.periods = {
+      '2019-05-20': {startingDay: true, color: '#d71a3a', textColor: '#ffffff'},
+      ...this.markDates(this.getSundaysInYear())
+    }
   }
+
 
   componentDidMount(){
     console.log('componentDidMount')
@@ -22,21 +35,65 @@ export default class PeriodScreen extends Component {
      this.setState({numofperiod: periodData.numofperiod}),
      this.setState({lengthofperiod: periodData.lengthofperiod}),
      this.setState({perioddate: periodData.perioddate})
+
+     //calling setState many times is very bad here
+     //better call it once like this
+     //this.setState({
+      // numofperiod: periodData.numofperiod,
+      // lengthofperiod: periodData.lengthofperiod,
+      // perioddate: periodData.perioddate
+    // })
     })
   }
 
-  periods = () => {
-	    
-	      //periods[perioddatetwo] = {selected: true, endingDay: true, color: '#d71a3a', textColor: '#ffffff'}
-	
-	   // }
-	}
+  //periods = () => {
 
-  
+	      //periods[perioddatetwo] = {selected: true, endingDay: true, color: '#d71a3a', textColor: '#ffffff'}
+
+	   // }
+	//}
+
+  //this is the function (markDates) that gives us the dates to mark
+  //the function that you needed should be similar to this
+  //it's simple, it has to just return an object since
+  //markedDates wants an object
+  markDates(datesArray){
+    let markedDates = {}
+    for (let i in datesArray){
+      markedDates[datesArray[i]] = {selected: true, color: '#d71a3a', textColor: '#ffffff'}
+    }
+    return markedDates
+  }
+
+  getSundaysInMonth(year, month){
+    let sundays = []
+    year = parseInt(year)
+    month = parseInt(month)
+    //small algorithm that will get us the sundays
+    //loop through the days of the given month in the given year
+    //and check if a day is a 0, which will mean it's a sunday
+    //to understand it more try it in the console browser
+    //or add more console.log to debug and see what's going on
+    for (let i=1; i<=new Date(year, month, 0).getDate(); i++){
+      let date = new Date(year, month-1, i)
+      if (date.getDay() == 0)
+        sundays.push(moment(date).format("YYYY-MM-D"))
+    }
+    return sundays
+  }
+
+  getSundaysInYear(year=2019){
+    let sundays = []
+    for (let i =1; i<=12; i++){
+      sundays = [...sundays, ...this.getSundaysInMonth(year, i)]
+    }
+    return sundays
+  }
 
   render() {
-    let {numofperiod, lengthofperiod, perioddate} = this.state
-    var periods = {}
+    let {numofperiod, lengthofperiod, perioddate } = this.state
+    //alert(JSON.stringify(this.periods))
+    //var periods = {}
 	    //var pickeddate = moment(this.state.perioddate).format('D');
 	    //var num = parseInt(pickeddate, 10);
 	    //var numperiod = parseInt(this.state.numofperiod, 10);
@@ -49,8 +106,8 @@ export default class PeriodScreen extends Component {
 	    //let i;
 	    //for(i=0; i < this.state.numofperiod; i++){
 	      //periods[this.state.perioddate] = {startingDay: true, color: '#d71a3a', textColor: '#ffffff'}
-	      periods['2019-05-20'] = {startingDay: true, color: '#d71a3a', textColor: '#ffffff'}
-    
+	      //periods['2019-05-20'] = {startingDay: true, color: '#d71a3a', textColor: '#ffffff'}
+
     return (
       <View style={styles.container}>
         <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#263c91', '#6f82c6', '#d71a3a']} style={{height: 80,}}>
@@ -68,12 +125,12 @@ export default class PeriodScreen extends Component {
             <View style={{alignItems:  'center', justifyContent: 'center',}}>
               <Text style={{textAlign: 'center', color: '#ffffff', fontSize: 20 }}>Period Calculator</Text>
             </View>
-          
+
           </View>
         </LinearGradient>
 
         <View style={{flex:1,}}>
-          
+
           <View style={{borderBottomColor: '#6f82c6', borderBottomWidth: 2, marginVertical: 10, }}>
             <View style={{flexDirection: 'row', paddingHorizontal: 10, justifyContent: 'space-between',}}>
               <View style={{alignItems: 'flex-start', justifyContent: 'flex-end',}}>
@@ -86,7 +143,7 @@ export default class PeriodScreen extends Component {
                 </TouchableOpacity>
               </View>
             </View>
-          </View>  
+          </View>
 
           <View style={{flex: 1, paddingHorizontal: 10,}}>
             <Calendar
@@ -103,7 +160,7 @@ export default class PeriodScreen extends Component {
               // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
               monthFormat={'dd / MMM / yyyy'}
               // Handler which gets executed when visible month changes in calendar. Default = undefined
-              onMonthChange={(month) => {}}
+              //onMonthChange={(month) => this.monthChange(month)}
               // Hide month navigation arrows. Default = false
               hideArrows={false}
               // Replace default arrows with custom ones (direction can be 'left' or 'right')
@@ -122,7 +179,7 @@ export default class PeriodScreen extends Component {
               showWeekNumbers={false}
               // Handler which gets executed when press arrow icon left. It receive a callback can go back month
               onPressArrowLeft={substractMonth => substractMonth()}
-              // Handler which 
+              // Handler which
 
               //gets executed when press arrow icon left. It receive a callback can go next month
               onPressArrowRight={addMonth => addMonth()}
@@ -134,11 +191,11 @@ export default class PeriodScreen extends Component {
               // Set custom calendarWidth.
               calendarWidth={320}
 
-              markedDates={periods}
+              markedDates={this.periods}
               // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
               markingType={'period'}
 
-              
+
               // Specify theme properties to override specific styles for calendar parts. Default = {}
               theme={{
                 backgroundColor: '#ffffff',
@@ -164,7 +221,7 @@ export default class PeriodScreen extends Component {
 
           />
           </View>
-             
+
         </View>
       </View>
     );
